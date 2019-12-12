aa2d81df60506f28a1be1f5da49a15866c662d16
DanLaZ <lazenberry_daniel@yahoo.com>
2019-12-12 14:26:04 -0500

    Added a window that will grab the data and display it *Still need to code that function*. Added the heart selections section *need to code the functions for that as well*.

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+
+
+// const picked_heart = document.querySelector('li.picked-heart');
+// picked_heart.className = 'animated bounce infinite';
+
+const picked_heart = document.getElementsByClassName('picked-heart');
+picked_heart.className = 'animated bounce infinite';
+
+console.log(picked_heart);
