d668d41405e609e198299a298ddd5fc2cee1b6f5
DanLaZ <lazenberry_daniel@yahoo.com>
2019-12-13 11:54:31 -0500

    Animations are added through javascript for the heart options

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,21 @@
 // const picked_heart = document.querySelector('li.picked-heart');
 // picked_heart.className = 'animated bounce infinite';
 
-const picked_heart = document.getElementsByClassName('picked-heart');
-picked_heart.className = 'animated bounce infinite';
+const picked_heart = document.querySelectorAll('.picked-heart');
+
+
+// On click Function for selecting a heart color
+const heartClick = (e) => {
+    for (let i = 0; i < picked_heart.length; i++) {
+        picked_heart[i].classList.add('animated', 'bounce');
+        console.log('I ran');
+    }
+    e.preventDefault();
+}
+
+
+// picked_heart.className = 'animated bounce infinite';
+
+heartClick();
 
 console.log(picked_heart);
